124f831006561fdb14690b7816533b4586e0627f
Kacper Ruściński <ruscin13@gmail.com>
2020-09-10 18:55:30 +0200

    testing

--- posenet/demos/skirt.js
+++ posenet/demos/skirt.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
   75,
@@ -12,26 +13,25 @@ var camera = new THREE.PerspectiveCamera(
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+renderer.setClearColor(0xd3d3d3);
 
-var loader = new GLTFLoader();
-loader.load(
-  "/models/ready-models/helmet.gltf",
-  function (gltf) {
-    console.log(gltf.scene.children[0]);
-    var skirt = gltf.scene.children[0];
-    skirt.scale.set(0.5, 0.5, 0.5);
-    //scene.add(skirt);
-    skirt.position.z = -10;
+var loader = new GLTFLoader().setPath("models/dress/");
+loader.load("dress.gltf", function (gltf) {
+  gltf.scene.traverse(function (child) {
+    if (child.isMesh) {
+      // TOFIX RoughnessMipmapper seems to be broken with WebGL 2.0
+      // roughnessMipmapper.generateMipmaps( child.material );
+    }
+  });
 
-    scene.add(gltf.scene);
-  },
-  undefined,
-  function (error) {
-    console.error(error);
-  }
-);
+  scene.add(gltf.scene);
 
-camera.position.z = 5;
+  roughnessMipmapper.dispose();
+
+  render();
+});
+
+camera.position.z = 5000;
 
 var animate = function () {
   requestAnimationFrame(animate);
