87837482d6f1e4dc3d2547b1910057be7b99171e
Kacper Ruściński <ruscin13@gmail.com>
2020-09-10 19:27:23 +0200

    testing continues

--- posenet/demos/skirt.js
+++ posenet/demos/skirt.js
@@ -1,7 +1,6 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
   75,
@@ -10,28 +9,31 @@ var camera = new THREE.PerspectiveCamera(
   1000
 );
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({ alpha: false });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor (0xff0000, 1);
 document.body.appendChild(renderer.domElement);
-renderer.setClearColor(0xd3d3d3);
+renderer.outputEncoding = THREE.sRGBEncoding;
 
-var loader = new GLTFLoader().setPath("models/dress/");
-loader.load("dress.gltf", function (gltf) {
-  gltf.scene.traverse(function (child) {
-    if (child.isMesh) {
-      // TOFIX RoughnessMipmapper seems to be broken with WebGL 2.0
-      // roughnessMipmapper.generateMipmaps( child.material );
-    }
-  });
+var loader = new GLTFLoader();
+loader.load(
+  "/models/dress/clothes_no_back.gltf",
+  function (gltf) {
+    console.log(gltf.scene.children[0]);
+    var skirt = gltf.scene.children[0];
+    skirt.scale.set(0.5, 0.5, 0.5);
+    scene.add(skirt);
+    skirt.position.z = -10;
 
-  scene.add(gltf.scene);
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
 
-  roughnessMipmapper.dispose();
-
-  render();
-});
-
-camera.position.z = 5000;
+camera.position.z = 100000;
 
 var animate = function () {
   requestAnimationFrame(animate);
